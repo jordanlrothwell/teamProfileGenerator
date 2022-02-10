a26759d078dc4da5d89a99a97ccf261714c4abde
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-02-10 18:45:04 +1000

    Added getName() method/test.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,12 +2,17 @@ const Employee = require('../lib/Employee.js')
 
 describe("Employee", () => {
   describe("Initialisation", () => {
-    it("should create an object with the following properties (if provided valid arguments): name; id; email", () => {
-      const john = new Employee("John", 1, "john@gmail.com");
+    it("should create an object with the following properties (if provided valid arguments): 1) name; 2) id; 3) email", () => {
+      const john = new Employee("John", 123, "john@gmail.com");
 
       expect(john.name).toEqual("John");
-      expect(john.id).toEqual(1);
+      expect(john.id).toEqual(123);
       expect(john.email).toEqual("john@gmail.com");
     });
+    it("should give that object a getName() method which returns its name property", () => {
+      const johnson = new Employee("Johnson", 456, "johnson@gmail.com");
+
+      expect(johnson.getName()).toEqual("Johnson");
+    })
   });
 });
