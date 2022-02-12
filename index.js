3a6334c85958d019a1bacb3b640199c68aea4ae1
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-02-12 13:50:08 +1000

    createEmployee() now adds the resulting instance to an employees.json file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import packages
 const fs = require("fs");
 const inquirer = require("inquirer");
+const path = require("path");
 
 // import modules
 const Employee = require("./lib/Employee");
@@ -9,15 +10,35 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const questions = require("./lib/questions");
 
-// inquirer prompts
+// get existing employee data
+const employeesJSON = fs.readFileSync(path.resolve(__dirname, "./data/employees.json"));
+const employeesObj = JSON.parse(employeesJSON);
+
+// function to create new employee instance + add it to the employees.json file
 const createEmployee = async function () {
   const employeeInfo = await inquirer.prompt(questions);
 
   if (employeeInfo.employeeType == "Manager") {
     const { name, id, email, officeNumber } = employeeInfo;
-    const newManager = new Manager(name, id, email, officeNumber);
-    console.log(newManager);
+    var newEmployee = new Manager(name, id, email, officeNumber);
+  } else if (employeeInfo.employeeType == "Engineer") {
+    const { name, id, email, github } = employeeInfo;
+    var newEmployee = new Engineer(name, id, email, github);
+  } else {
+    const { name, id, email, school } = employeeInfo;
+    var newEmployee = new Intern(name, id, email, school);
   }
+
+  employeesObj.push(newEmployee);
+
+  const updatedEmployees = JSON.stringify(employeesObj);
+
+  fs.writeFile("./data/employees.json", updatedEmployees, (err) => {
+    if (err) {
+      console.log("Failed to successfully update employees.json");
+    }
+    console.log("Successfully updated employees.json!");
+  });
 };
 
-createEmployee();
+createEmployee()
