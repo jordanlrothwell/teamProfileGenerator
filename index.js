1a72e0b79d78aead22b90ac76ba6991e9b7302ee
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-02-12 10:45:16 +1000

    Created index.js (currently only imports packages/modules); created questions for inquirer.

---
+++ index.js
@@ -0,0 +1,8 @@
+// import modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+// import our custom classes
+const Employee = require("./lib/Employee")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
