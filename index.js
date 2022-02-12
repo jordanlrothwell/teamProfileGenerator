686efda2802dbad33bf7295b57b4898a882fcfb2
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-02-12 11:03:58 +1000

    Fixed dynamic question behaviour so separate module would function.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// import modules
+// import packages
 const fs = require("fs");
 const inquirer = require("inquirer");
-// import our custom classes
-const Employee = require("./lib/Employee")
-const Manager = require("./lib/Manager")
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
+
+// import modules
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const questions = require("./lib/questions");
+const generalQuestions = questions[0];
+const conditionalQuestions = questions[1];
+
+// inquirer prompt
+// const getGeneralInfo = async function () {
+//     await inquirer.prompt(generalQuestions);
+//     console.log(answers)
+// }
+
+// getGeneralInfo();
